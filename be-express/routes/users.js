75f335c419fa07a9caf9a1393209d1601e339291
HaDucDev <haductrung392000@gmail.com>
2023-06-03 17:01:00 +0700

    update-structure-project

--- be-express/routes/users.js
+++ be-express/routes/users.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const models = require("../models");
+
+const UserController = require('../controller/UserController')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  models.User.findAll().spread(function(user, created){
-    res.send({"user": user, "created": created});
-  });
-});
+router.get('/', UserController.getAll);
 
 module.exports = router;
